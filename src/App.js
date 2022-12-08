edca9a3297572e56ba6ee5f26cc346790643e0f9
komailhaider22 <61079441+komailhaider22@users.noreply.github.com>
2022-12-08 08:09:17 -0800

    Revert "Ammend App.js"
    This reverts commit 5de52d22dd37cc120b85afb56cc1397476cd949d.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,9 @@ import "./App.css";
 import Navbar from "./Layout/Navbar";
 
 function App() {
-  var nums = [1, 2, 1];
-
-  var arr = []; // arr
-  let i = 0;
-  let n = nums.length;
-  for (i; i < n; i++) {
-    arr.push(nums[i]); //arr
-  }
-  const ans = arr.concat(nums);
-
-  console.log(ans);
-
-  // for (j; j < names.length; j++) {
-  //   sortedHeights[j] = heights[j];
-  // }
-  // console.log(sortedHeights);
-
   return (
     <>
-      {/* //<Navbar></Navbar> */}
-      <div>Hi</div>
+      <Navbar></Navbar>
     </>
   );
 }
